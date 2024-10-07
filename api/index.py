35de414327f25c62b6cd7b98bd151c4d757e13d4
Tyler-Dunning <118504218+Tyler-Dunning@users.noreply.github.com>
2024-10-07 16:12:00 -0700

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv() 
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return os.getenv("MY_KEY")
 
-@app.route('/about')
-def about():
-    return 'About'
+uri = f'mongodb+srv://tdunning111:{ os.getenv("MY_KEY") }@ratings-cluster.1rltz.mongodb.net/?retryWrites=true&w=majority&appName=Ratings-Cluster'
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
